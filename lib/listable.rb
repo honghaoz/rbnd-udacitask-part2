7975e1f2bfb4e5969f1948dc5a42ede96b24aafd
Honghao Zhang <zhh358@gmail.com>
2016-05-20 00:40:25 -0400

    finish format_date

--- lib/listable.rb
+++ lib/listable.rb
@@ -4,8 +4,15 @@ module Listable
     "#{description}".ljust(30)
   end
 
-  def format_date(date)
-
+  def format_date(options = {})
+    if options.key?(:start_date) || options.key?(:end_date)
+      dates = start_date.strftime('%D') if start_date
+      dates << ' -- ' + end_date.strftime('%D') if end_date
+      dates = 'N/A' if !dates
+      dates
+    elsif options.key?(:due)
+      due ? due.strftime('%D') : 'No due date'
+    end
   end
 
   def format_priority(priority)
--- lib/todo.rb
+++ lib/todo.rb
@@ -8,21 +8,17 @@ class TodoItem
     @priority = options[:priority]
   end
 
-  def format_date
-    @due ? @due.strftime('%D') : 'No due date'
-  end
-
   def format_priority
     value = ' ⇧' if @priority == 'high'
     value = ' ⇨' if @priority == 'medium'
     value = ' ⇩' if @priority == 'low'
     value = '' if !@priority
-    return value
+    value
   end
 
   def details
     format_description(@description) + 'due: ' +
-        format_date +
+        format_date(due: @due) +
         format_priority
   end
 
